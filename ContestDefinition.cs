1987424be8e84a904f6d43be84cb8efa73e19ea0
MarcosFelipeBC <marcosfellipecosta@gmail.com>
2020-01-19 03:27:26 -0300

    First blood

---
+++ ContestDefinition.cs
@@ -0,0 +1,29 @@
+using System.Runtime.Serialization;
+
+namespace contests_finder
+{
+    [DataContract]
+    public class ContestDefinition
+    {
+        [DataMember(Name="id")]
+        public int Id { get; set; }
+
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name="type")]
+        public string Type { get; set; }
+
+        [DataMember(Name="phase")]
+        public string Phase { get; set; }
+
+        [DataMember(Name="durationSeconds")]
+        public int DurationSeconds { get; set; }
+
+        [DataMember(Name="startTimeSeconds")]
+        public int StartTimeSeconds { get; set; }
+
+        [DataMember(Name="relativeTimeSeconds")]
+        public int RelativeTimeSeconds { get; set; }
+    }
+}
